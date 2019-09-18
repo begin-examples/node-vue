b05c54f1ab34671aa91fd9497828a6827d79695c
kj <kristoferjoseph@gmail.com>
2019-09-18 11:49:33 -0700

    Update attempts to delete progress plugin all together.

--- vue.config.js
+++ vue.config.js
@@ -9,13 +9,6 @@ module.exports = {
   chainWebpack: config => {
     // remove vue-cli-service's progress output
     config.plugins.delete('progress')
-    // optionally replace with another progress output plugin
-    // `npm i -D simple-progress-webpack-plugin` to use
-    config.plugin('simple-progress-webpack-plugin').use(require.resolve('simple-progress-webpack-plugin'), [
-      {
-        format: 'minimal', // options are minimal, compact, expanded, verbose
-      },
-    ])
   },
   configureWebpack: {
     stats: 'errors-only'
